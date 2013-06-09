c514b9062bba2c31360fd95b15e7eae2de57a05f
Ryan Closner <ryan@ryanclosner.com>
2013-06-08 22:19:00 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var texture_path = require('painterly-textures')(__dirname)
+var create_game  = require('voxel-hello-world')
+
+var game = create_game({
+  texturePath: texture_path,
+  materialFlatColor: false,
+  materials: [
+    ['grass', 'dirt', 'grass_dirt'],
+    'obsidian',
+    'brick',
+    'grass',
+    'plank'
+  ],
+  playerSkin: './img/player.png',
+  generateVoxelChunks: false,
+  chunkDistance: 1
+})
+
+window.game = game
